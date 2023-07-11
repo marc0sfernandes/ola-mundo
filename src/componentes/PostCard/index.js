a10891b07c357ea02880d3b7b57c2b21a056d2e8
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-07-11 13:26:40 -0300

    create component scroll to top

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -6,7 +6,7 @@ export default function PostCard({ post }) {
     return (
         <Link to={`/posts/${post.id}`}>
             <div className={styles.post}>
-                <img className={styles.capa} src={`assets/posts/${post.id}/capa.png`} alt='Imagem de capa do post' />
+                <img className={styles.capa} src={`/assets/posts/${post.id}/capa.png`} alt='Imagem de capa do post' />
                 <h2 className={styles.titulo}>{post.titulo}</h2>
                 <BotaoPrincipal>Ler</BotaoPrincipal>
             </div>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Rodape from "componentes/Rodape";
+import ScrollToTop from "componentes/ScrollToTop";
 import NaoEncontrada from "paginas/NaoEncontrada";
 import Post from "paginas/Post";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -10,6 +11,7 @@ import SobreMim from "./paginas/SobreMim";
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
 
       <Routes>
@@ -17,7 +19,7 @@ function AppRoutes() {
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
         </Route>
-        
+
         <Route path="posts/:id" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
